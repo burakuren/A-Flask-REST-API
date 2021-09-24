0b06e8416bff24b9c6438d78d199ae040dc11439
Burak Uren <burakuren101@gmail.com>
2021-09-24 12:15:43 +0300

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from db import db
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -14,9 +14,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt = JWT(app, authenticate, identity)
 
@@ -27,6 +25,8 @@ api.add_resource(ItemList, "/items")
 
 api.add_resource(UserRegister, "/register")
 
-db.init_app(app)
+
 if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
