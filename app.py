b973e64135656185ff269ba32c8c77861d9cf107
Burak Uren <burakuren101@gmail.com>
2021-09-26 19:34:48 +0300

    Updates

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import os
-# import re
-
-
-from dotenv import load_dotenv
-load_dotenv()
 
 from flask import Flask
 from flask_restful import Api
@@ -14,17 +9,16 @@ from resources.user_resource import UserRegister
 from resources.store_resource import Store, StoreList
 from resources.item_resource import Item, ItemList
 
-def _import():
-    from run import create_tables
-"""
+
+
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-"""
+
 
 app = Flask(__name__)
 app.secret_key = "jose"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("CONN_STR")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("uri")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
 
@@ -40,7 +34,6 @@ api.add_resource(UserRegister, "/register")
 
 
 if __name__ == "__main__":
-    _import()
     from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
